6a4056cb08c0669138e34ae549dba48a67d2dd4f
Susmita Basak <susum05@gmail.com>
2019-09-28 15:09:02 +0530

    added setter getter method

--- book.java
+++ book.java
@@ -1,4 +1,18 @@
 class Book{
 	private int bookId;
 	private String name;
+	
+	public void get(){
+		this.bookId = bookId;
+	}
+	public int set(bookId){
+		return bookId;
+	}
+	public void get(){
+		this.name =name;
+		
+	}
+	public int set(name){
+		return name;
+	}
 	}
